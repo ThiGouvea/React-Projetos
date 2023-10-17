5c737b7862d0185bcbdc563354fcaa8a8a86462e
Thiagouvea <thiagouvea90@gmail.com>
2023-10-17 10:29:59 -0300

    adcinado header

--- ola-mundo-SPA/src/App.js
+++ ola-mundo-SPA/src/App.js
@@ -1,10 +1,13 @@
 import SobreMim from "./Paginas/SobreMim";
 import Inicio from "./Paginas/Inicio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./Componentes/Menu";
 
 function App() {
   return (
     <BrowserRouter>
+      <Menu />
+
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/sobremim" element={<SobreMim />}/>
--- ola-mundo-SPA/src/Componentes/Menu/index.js
+++ ola-mundo-SPA/src/Componentes/Menu/index.js
@@ -1,6 +1,22 @@
+import { Link, useLocation } from 'react-router-dom'
+import styles from './Menu.module.css'
+
 const Menu = () => {
+    const localizacao = useLocation();
+    console.log(localizacao);
+
     return (
-        "Menu de Navegação"
+        <header>
+            <nav className={styles.navegacao}>
+                <Link className={styles.link} to='/'>
+                    Inicio
+                </Link>
+                <Link className={styles.link} to='/sobremim'>
+                    Sobre Mim
+                </Link>
+
+            </nav>
+        </header>
     )
 }
 
