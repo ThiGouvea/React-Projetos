b17a998bcd390eb1eacc69180139d795a869adab
Thiagouvea <thiagouvea90@gmail.com>
2023-10-20 11:36:56 -0300

    iniciando o projeto para aprendizado de tailwindcss

---
+++ tailwindvite/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div>
+            <span>Olá, Usuario</span>
+            <h1>Alura newsletter</h1>
+            <input type="checkbox" />
+        </div>
+    )
+}
+
+export default Header
