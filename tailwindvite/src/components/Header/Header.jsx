a7fdcef186a421e7a67fa4d817968757a66a037d
Thiagouvea <thiagouvea90@gmail.com>
2023-10-23 11:32:07 -0300

    adcionando classes dinamicas e modo noturno

--- tailwindvite/src/components/Header/Header.jsx
+++ tailwindvite/src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
+import ToggleTheme from "../ToggleTheme/ToggleTheme"
+
 const Header = () => {
     return (
-        <div>
-            <span>Olá, Usuario</span>
-            <h1>Alura newsletter</h1>
-            <input type="checkbox" />
+        <div 
+        className=" flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+            <span className="text-gray-100">Olá, Usuario</span>
+            <h1 className="text-gray-100 text-xl">Alura newsletter</h1>
+            <ToggleTheme />
         </div>
     )
 }
 
 export default Header
+
+//react
